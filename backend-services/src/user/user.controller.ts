3f02a60f84f2a218e40894ee48b65a8fd99a4781
didimitriiii <dimitristrovsk@gmail.com>
2025-05-01 22:09:54 -0300

    Add backend-services/src/user/user.controller.ts 1746148193474

---
+++ backend-services/src/user/user.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(Number(id));
+  }
+
+  @Post()
+  create(@Body() createUserDto: { username: string; role: string }) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: Partial<{ username: string; role: string }>) {
+    return this.userService.update(Number(id), updateUserDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(Number(id));
+  }
+}
